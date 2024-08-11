b7e8597f5cc975841c64ca7de90b587dae8ccdec
Evan Barberousse <evanlegamer@gmail.com>
2024-08-11 18:49:05 -0400

    Moved from arn list to a single addon

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ install_zip_dependencies(){
 
 publish_dependencies_as_layer(){
 	echo "Publishing dependencies as a layer..."
- 	local result
+	local result
 	result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip --compatible-runtimes "python${INPUT_PYTHON_VERSION}" --compatible-architectures "x86_64")
 	LAYER_VERSION=$(jq '.Version' <<< "$result")
 	rm -rf python
@@ -25,37 +25,29 @@ publish_function_code(){
 
 update_function_layers(){
 	echo "Using the layer in the function..."
- 	local function_state
-  	local function_status
+	local function_state
+	local function_status
 	function_state=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.State')
- 	function_status=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.LastUpdateStatus')
+	function_status=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.LastUpdateStatus')
 	while [[ $function_state != "\"Active\"" && $function_status != "\"Successful\"" ]]
- 	do
+	do
 		function_state=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.State')
-  		function_status=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.LastUpdateStatus')
+		function_status=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.LastUpdateStatus')
 		sleep 1
 	done
- 	echo "The Function State is: $function_state"
- 	echo "The Function Status is: $function_status"
+	echo "The Function State is: $function_state"
+	echo "The Function Status is: $function_status"
 
- 	# Prepare the new layer ARN
- 	local new_layer="${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
+	# Prepare the new layer ARN
+	local new_layer="${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
 
- 	# Convert the comma-separated list of addon layers into an array
- 	IFS=',' read -r -a addon_layers_array <<< "$addon_layer_arns"
+	# Add the single addon layer ARN
+	local complete_layers="${new_layer},${addon_layer_arns}"
 
- 	# Construct the complete list of layers, including the new layer
- 	local layers_list=("$new_layer")
- 	for addon_layer in "${addon_layers_array[@]}"; do
- 	    layers_list+=("$addon_layer")
- 	done
+	echo "Updating the function with the new layers configuration: $complete_layers"
 
- 	# Join the layers list into a comma-separated string
- 	local layers=$(IFS=,; echo "${layers_list[*]}")
-	echo "Updating the function with the following layers: $layers"
-
- 	# Update the Lambda function with the new layers configuration
-	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers $layers
+	# Update the Lambda function with the new layers configuration
+	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers $complete_layers
 }
 
 deploy_lambda_function(){
